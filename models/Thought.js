07c7131bb9ca993fd6da2606e6adeebff161115b
aldrinburgos <burgos.aldrin@gmail.com>
2021-02-21 16:41:28 -0600

    create Reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -18,6 +18,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true,
     },
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
@@ -31,6 +32,35 @@ const ThoughtSchema = new Schema(
 ThoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minlength: [1],
+      maxlength: [280],
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const Thought = model("Thought", ThoughtSchema);
 
